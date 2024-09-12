24e3e6ba4233a50c3d85aa002e3b1c2f25d82f71
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-12 12:46:03 -0400

    Person test was added to Pers.test.js and Person class was created and passed all test

---
+++ __tests__/classes/Person.test.js
@@ -0,0 +1,31 @@
+const Person = require("../../classes/Person");
+const Bag = require("../../classes/Bag")
+
+describe("Person property test", () => {
+  const person = new Person("thomas", "Maldive");
+  test("Person class should create Person instance", () => {
+    expect(person).toBeInstanceOf(Person);
+  });
+  test("name should be a string", () => {
+    expect(typeof person.name).toBe("string");
+  });
+  test("destination should be a string", () => {
+    expect(typeof person.destination).toBe("string");
+  });
+  test("bags array initial value should be empty array", () => {
+    expect(person.bags).toEqual([]);
+  });
+});
+
+describe("Person Methods", () => {
+  const person = new Person("thomas", "Maldive");
+
+  test("addBag(bag) should Updates the bags array with a bag.", () => {
+    const bag = new Bag()
+    person.addBag(bag);
+    expect(person.bags.includes(bag)).toBe(true);
+  });
+  test("getBags() should Returns the array of bags.", () => {
+    expect(person.getBags()).toBe(person.bags);
+  });
+});
